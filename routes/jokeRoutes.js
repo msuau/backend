0c232ce0ef04ebbb025d4f92544643cd978dd120
msuau <mat.suau@orange.fr>
2025-06-14 01:56:06 +0200

    getRandomJoke fix 5

--- routes/jokeRoutes.js
+++ routes/jokeRoutes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const jokeController = require('../controllers/jokeController');
 
+router.get('/random', jokeController.getRandomJoke);   // GET /api/v1/jokes/random
 router.post('/', jokeController.addJoke);              // POST /api/v1/jokes/
 router.get('/', jokeController.getAllJokes);           // GET /api/v1/jokes/
 router.get('/:id', jokeController.getJokeById);        // GET /api/v1/jokes/:id
-router.get('/random', jokeController.getRandomJoke);   // GET /api/v1/jokes/random
+  
 
 module.exports = router;
